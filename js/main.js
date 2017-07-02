24977da0e9f79d1145595f9e77aa8be9b4827047
N.S.Devaraj <nsdevaraj@gmail.com>
2017-07-02 12:59:55 +0530

    update

--- js/main.js
+++ js/main.js
@@ -300,12 +300,13 @@ angular.module('invoicing',  ['cmGoogleApi'])
 			cookie_policy: 'single_host_origin',
 			fetch_basic_profile: true
 		})
+		
 		.setClientId('598025943249-e0lk9nun0qsg054q3pi59his8eugsnjt.apps.googleusercontent.com')
 /*		.addScope('https://www.googleapis.com/auth/drive.readonly')
 		.addScope(' https://www.googleapis.com/auth/youtube')*/
 		
-        .addScope("https://www.googleapis.com/auth/spreadsheets")
-		.addScope("https://www.googleapis.com/auth/drive.metadata.readonly")
-		.addApi('drive', 'v2')
+        .addScope('https://www.googleapis.com/auth/spreadsheets')
+		.addScope('https://www.googleapis.com/auth/drive.metadata.readonly')
+		.addApi('drive', 'v4')
 		.loadPickerLibrary();
 });
